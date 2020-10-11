effb4a3e819700590c7523e716d5ad0b7a396396
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-10-11 16:44:14 +0300

    Created addMasechtos and printList methods

--- src/LinkedList2/FollowAlong/Demo.java
+++ src/LinkedList2/FollowAlong/Demo.java
@@ -1,2 +1,25 @@
-package LinkedList2.FollowAlong;public class Demo {
+package LinkedList2.FollowAlong;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedList<String> masechtosToLearn = new LinkedList<String>();
+        addMasechtos(masechtosToLearn);
+        printList(masechtosToLearn);
+    }
+
+    public static void addMasechtos(LinkedList<String> ll){
+        ll.add("Brachos");
+        ll.add("Shabbos");
+        ll.add("Pesachim");
+    }
+
+    public static void printList(LinkedList<String> ll){
+        Iterator<String> iter = ll.iterator();
+        while(iter.hasNext()){
+            System.out.println("Now learning " + iter.next());
+        }
+    }
 }
