28457575c28da090ab343c0c365af2bc1808f3b9
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2020-10-11 17:38:50 +0300

    Added removeAll() method

--- src/LinkedList2/FollowAlong/Demo.java
+++ src/LinkedList2/FollowAlong/Demo.java
@@ -2,11 +2,14 @@ package LinkedList2.FollowAlong;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     public static void main(String[] args) {
         LinkedList<String> masechtosToLearn = new LinkedList<String>();
-        addMasechtos(masechtosToLearn);
+        masechtosToLearn.add("Brachos");
+        masechtosToLearn.add("Shabbos");
+        masechtosToLearn.add("Pesachim");
         printList(masechtosToLearn);
         masechtosToLearn.add(2, "Eiruvin");
         printList(masechtosToLearn);
@@ -14,19 +17,42 @@ public class Demo {
         printList(masechtosToLearn);
         masechtosToLearn.remove(2);
         printList(masechtosToLearn);
+        removeAll(masechtosToLearn);
+        printList(masechtosToLearn);
     }
 
-    public static void addMasechtos(LinkedList<String> ll){
-        ll.add("Brachos");
-        ll.add("Shabbos");
-        ll.add("Pesachim");
+    public static void removeAll(LinkedList<String> ll){
+        for(int index = ll.size() - 1; index >= 0; index--){
+            ll.remove(index);
+        }
+    }
+
+    public static boolean addAlphabetically(LinkedList<String> ll, String element){
+        ListIterator<String> iter = ll.listIterator();
+        while(iter.hasNext()){
+            int comp = iter.next().compareTo(element);
+            if (comp == 0) return false;
+            else if(comp > 0){
+                // new element should appear before current element
+                iter.previous();
+                break;
+            } else {
+                continue;
+            }
+        }
+        iter.add(element);
+        return true;
     }
 
     public static void printList(LinkedList<String> ll){
-        Iterator<String> iter = ll.iterator();
-        while(iter.hasNext()){
-            System.out.println("Now learning " + iter.next());
+        if(ll.size() > 0) {
+            Iterator<String> iter = ll.iterator();
+            while (iter.hasNext()) {
+                System.out.println("Now learning " + iter.next());
+            }
+            System.out.println("Okay, we're done.\n");
+        } else {
+            System.out.println("No elements found.");
         }
-        System.out.println("Okay, we're done.\n");
     }
 }
