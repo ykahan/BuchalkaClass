17aa0625837821b608f31d81b80eae184c1279d0
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-14 14:27:37 +0200

    Added Reference Types and Value Types package

---
+++ src/ReferenceTypesAndValueTypes/Main.java
@@ -0,0 +1,28 @@
+package ReferenceTypesAndValueTypes;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int myInt1 = 10;
+        int myInt2 = myInt1;
+        System.out.println(myInt1);
+        System.out.println(myInt2);
+        System.out.println(myInt1 == myInt2);
+
+        myInt2++;
+        System.out.println(myInt1);
+        System.out.println(myInt2);
+        System.out.println(myInt1 == myInt2);
+
+        int[] intArray1 = new int[5];
+        int[] intArray2 = intArray1;
+
+        System.out.println(Arrays.toString(intArray1));
+        System.out.println(Arrays.toString(intArray2));
+        intArray2[2] = 2;
+        
+        System.out.println(Arrays.toString(intArray1));
+        System.out.println(Arrays.toString(intArray2));
+    }
+}
