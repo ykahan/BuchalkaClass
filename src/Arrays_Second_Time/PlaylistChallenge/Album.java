f2945498252f1683bb048b52375b3fe07a85e6df
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-22 17:16:13 +0200

    Built Album class

--- src/Arrays_Second_Time/PlaylistChallenge/Album.java
+++ src/Arrays_Second_Time/PlaylistChallenge/Album.java
@@ -1,4 +1,41 @@
 package Arrays_Second_Time.PlaylistChallenge;
 
+import java.util.ArrayList;
+
 public class Album {
+    private ArrayList<Song> songs = new ArrayList<>();
+    private String title;
+
+    public Album(ArrayList<Song> songs, String title) {
+        this.songs = songs;
+        this.title = title;
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public Song getSong(String name) {
+        for (int i = 0; i < songs.size(); i++) {
+            if (songs.get(i).getTitle().equals(name)) return songs.get(i);
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.title);
+        sb.append("\n");
+        if (songs.size() > 0) {
+            for (int i = 0; i < songs.size(); i++) {
+                sb.append((i + 1) + ")\t" + songs.get(i).getTitle() + "\n");
+            }
+        } else sb.append("No songs found");
+        return sb.toString();
+    }
 }
