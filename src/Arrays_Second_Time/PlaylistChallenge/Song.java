dd6e5b5dcf8116e8a0e76f12eeefdc94bb910cd6
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-22 17:09:48 +0200

    Built Song class

--- src/Arrays_Second_Time/PlaylistChallenge/Song.java
+++ src/Arrays_Second_Time/PlaylistChallenge/Song.java
@@ -1,4 +1,19 @@
 package Arrays_Second_Time.PlaylistChallenge;
 
 public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration){
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
 }
