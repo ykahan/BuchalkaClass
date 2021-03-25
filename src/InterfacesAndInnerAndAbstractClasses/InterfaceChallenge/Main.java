cb9a364fe29b776574c46b218db05200da3d5e95
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-25 08:04:50 +0200

    Improved testing code for Monster class by having it use a List rather than an ArrayList

--- src/InterfacesAndInnerAndAbstractClasses/InterfaceChallenge/Main.java
+++ src/InterfacesAndInnerAndAbstractClasses/InterfaceChallenge/Main.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Monster monster = new Monster();
-        ArrayList<String> monsterValues = new ArrayList<>();
+        List<String> monsterValues = new ArrayList<>();
         monsterValues.add("Lionman");
         monsterValues.add("50");
-        monsterValues.add("Poison breath");
+        monsterValues.add("Poison Breath");
         monsterValues.add("Garbage Pail Kid Cards");
         monster.populateFields(monsterValues);
         System.out.println(monster.toString());
