154be5cd2a7b629393f7fb7bb8d16cb0ace0cf99
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-25 07:30:11 +0200

    Implemented interface

--- src/InterfacesAndInnerAndAbstractClasses/InterfaceChallenge/ISaveable.java
+++ src/InterfacesAndInnerAndAbstractClasses/InterfaceChallenge/ISaveable.java
@@ -1,4 +1,10 @@
 package InterfacesAndInnerAndAbstractClasses.InterfaceChallenge;
 
+import java.util.List;
+
 public interface ISaveable {
+    List returnValues();
+    void populateFields(List list);
+    String toString();
+
 }
