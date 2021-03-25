594cb5f76a92f8298d1cbefc16257dbaf5cb2e03
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-25 13:04:32 +0200

    Provided GearBox with fields, a constructor, and an inner class Gear with its own fields and constructor

--- src/InterfacesAndInnerAndAbstractClasses/InnerClass/GearBox.java
+++ src/InterfacesAndInnerAndAbstractClasses/InnerClass/GearBox.java
@@ -1,4 +1,28 @@
 package InterfacesAndInnerAndAbstractClasses.InnerClass;
 
+import java.util.ArrayList;
+
 public class GearBox {
+    private ArrayList<Gear> gears;
+    private int maxGear;
+    private int currentGear;
+
+    public GearBox(int maxGear) {
+        this.maxGear = maxGear;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+
+    public class Gear {
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+    }
+
+
 }
