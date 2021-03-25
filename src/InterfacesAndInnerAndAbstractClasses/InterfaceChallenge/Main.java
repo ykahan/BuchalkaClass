511948673a1f66277935a2e47ef5712a233b38f9
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-25 08:09:05 +0200

    Added testing code for PlayerCharacter

--- src/InterfacesAndInnerAndAbstractClasses/InterfaceChallenge/Main.java
+++ src/InterfacesAndInnerAndAbstractClasses/InterfaceChallenge/Main.java
@@ -14,5 +14,21 @@ public class Main {
         monster.populateFields(monsterValues);
         System.out.println(monster.toString());
         List<String> restoredMonster = monster.returnValues();
+
+        PlayerCharacter pc = new PlayerCharacter();
+        List<String> pcValues = new ArrayList<>();
+        pcValues.add("Warren the Warrior");
+        pcValues.add("Warrior");
+        pcValues.add("Warren");
+        pcValues.add("15");
+        pcValues.add("Sword");
+        pcValues.add("Garbage Pail Kid Cards");
+        pcValues.add("Leather Shirt");
+        pcValues.add("Baseball Glove");
+        pcValues.add("20");
+        pc.populateFields(pcValues);
+        System.out.println(pc.toString());
+        List<String> restoredPc = pc.returnValues();
+
     }
 }
