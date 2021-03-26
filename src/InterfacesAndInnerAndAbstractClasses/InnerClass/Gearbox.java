d979849f2c9152e1f967959d54b7ba34952e8be1
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-26 08:38:27 +0300

    Made Gear class a private class, added code to Gearbox to perform basic manipulations of Gear

--- src/InterfacesAndInnerAndAbstractClasses/InnerClass/Gearbox.java
+++ src/InterfacesAndInnerAndAbstractClasses/InnerClass/Gearbox.java
@@ -6,6 +6,7 @@ public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGear;
     private int currentGear;
+    private boolean clutchIsIn;
 
     public Gearbox(int maxGear) {
         this.maxGear = maxGear;
@@ -14,7 +15,34 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public class Gear {
+    public void operateClutch(boolean in) {
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if (gearIsValid(number, ratio) && gearIsNew(number, ratio)) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int number){
+        this.currentGear = number;
+    }
+
+    private boolean gearIsNew(int number, double ratio) {
+        for (int i = 0; i < gears.size(); i++) {
+            Gear gear = gears.get(i);
+            if(number == gear.gearNumber) return false;
+            if(ratio == gear.ratio) return false;
+        }
+        return true;
+    }
+
+    private boolean gearIsValid(int number, double ratio) {
+        return number > 0 && number <= maxGear && ratio > 0;
+    }
+
+    private class Gear {
         private int gearNumber;
         private double ratio;
 
@@ -23,7 +51,7 @@ public class Gearbox {
             this.ratio = ratio;
         }
 
-        public double driveSpeed(int revs){
+        public double driveSpeed(int revs) {
             return revs * this.ratio;
         }
     }
--- src/InterfacesAndInnerAndAbstractClasses/InnerClass/Main.java
+++ src/InterfacesAndInnerAndAbstractClasses/InnerClass/Main.java
@@ -3,9 +3,7 @@ package InterfacesAndInnerAndAbstractClasses.InnerClass;
 public class Main {
     public static void main(String[] args) {
         Gearbox porsche = new Gearbox(6);
-        Gearbox.Gear first = porsche.new Gear(1, 12.3);
-        Gearbox.Gear second = porsche.new Gear(2, 6.5);
-        System.out.println(first.driveSpeed(1000));
-        System.out.println(second.driveSpeed(560));
+        porsche.addGear(1, 12.3);
+        porsche.addGear(2, 6.5);
     }
 }
