3f5326c7c4273f493a17bd1d22c4cae4ddbf80c1
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-03-26 15:25:06 +0300

    Added code to Button class, including interface OnClickListener

--- src/InterfacesAndInnerAndAbstractClasses/InnerClass/Button.java
+++ src/InterfacesAndInnerAndAbstractClasses/InnerClass/Button.java
@@ -1,4 +1,26 @@
 package InterfacesAndInnerAndAbstractClasses.InnerClass;
 
 public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title){
+        this.title = title;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener){
+        this.onClickListener = onClickListener;
+    }
+
+    public void onClick(){
+        this.onClickListener.onClick(this.title);
+    }
+
+    public interface OnClickListener{
+        public void onClick(String str);
+    }
 }
