1a8b85dd2cf3e3242fc29769b2b9f14bb705727a
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-07 15:01:07 +0300

    Copied LinkedList challenge to begin Inner Class challenge

---
+++ src/InterfacesAndInnerAndAbstractClasses/Challenge/PlaylistChallenge/Main.java
@@ -0,0 +1,24 @@
+package InterfacesAndInnerAndAbstractClasses.Challenge.PlaylistChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Playlist pl = new Playlist();
+        Album firstAlbum = new Album("first");
+        Album secondAlbum = new Album("second");
+
+        firstAlbum.addSong(new Song("a", 1.2));
+        firstAlbum.addSong(new Song("b", 2.3));
+        firstAlbum.addSong(new Song("c", 3.4));
+        firstAlbum.addSong(new Song("d", 4.5));
+
+        secondAlbum.addSong(new Song("z", 9.8));
+        secondAlbum.addSong(new Song("y", 8.7));
+        secondAlbum.addSong(new Song("x", 7.6));
+        secondAlbum.addSong(new Song("w", 6.5));
+
+        pl.addAlbum(firstAlbum);
+        pl.addAlbum(secondAlbum);
+
+        pl.operate();
+    }
+}
---
+++ src/InterfacesAndInnerAndAbstractClasses/Challenge/PlaylistChallenge/Song.java
@@ -0,0 +1,31 @@
+package InterfacesAndInnerAndAbstractClasses.Challenge.PlaylistChallenge;
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    private double getDuration() {
+        return duration;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Title: " + this.getTitle() + "\n");
+        sb.append("Duration: " + this.getDuration() + "\n");
+        for (int i = 0; i < 50; i++) {
+            sb.append("=");
+        }
+        sb.append("\n");
+        return sb.toString();
+    }
+}
