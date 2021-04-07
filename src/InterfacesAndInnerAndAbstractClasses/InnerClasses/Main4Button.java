1a134eafd5d44a8a457ac340cd3edca2a10e435e
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-07 14:15:13 +0300

    Built ClickListener as an example of a local class

--- src/InterfacesAndInnerAndAbstractClasses/InnerClasses/Main4Button.java
+++ src/InterfacesAndInnerAndAbstractClasses/InnerClasses/Main4Button.java
@@ -1,4 +1,41 @@
 package InterfacesAndInnerAndAbstractClasses.InnerClasses;
 
+import java.util.Scanner;
+
 public class Main4Button {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
+    public static void main(String[] args) {
+
+        class ClickListener implements Button.OnClickListener {
+            // local class, defined within a method
+
+            public ClickListener(){
+                System.out.println("I've been attached");
+            }
+
+            @Override
+            public void onClick(String str) {
+                System.out.println(str + " is clicked, fella");
+            }
+        }
+
+        btnPrint.setOnClickListener(new ClickListener());
+        listen();
+    }
+
+    private static void listen(){
+        boolean continuing = true;
+        while(continuing){
+            int choice = Integer.parseInt(scanner.nextLine());
+            switch(choice){
+                case 0:
+                    continuing = false;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
+    }
 }
