a7b17383813a449dd573cbdc0aac722ee68931d0
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-08 10:15:32 +0300

    Created template Cat class extending Animal

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Cat.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Cat.java
@@ -1,2 +1,27 @@
-package InterfacesAndInnerAndAbstractClasses.AbstractClasses;public class Cat {
+package InterfacesAndInnerAndAbstractClasses.AbstractClasses;
+
+public class Cat extends Animal{
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void breathe() {
+
+    }
+
+    @Override
+    public void walk() {
+
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
 }
