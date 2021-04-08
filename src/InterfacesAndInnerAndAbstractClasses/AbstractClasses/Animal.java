dcee9c200b72d529f9ef29ea663c47d42c5f0b81
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-08 10:55:26 +0300

    Added abstract String intro() signature to Animal

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Animal.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Animal.java
@@ -16,6 +16,7 @@ public abstract class Animal {
     public abstract void eat();
     public abstract void breathe();
     public abstract void walk();
+    public abstract String intro();
 
     public String getName() {
         return name;
