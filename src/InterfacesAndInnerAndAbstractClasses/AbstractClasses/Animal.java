c4363db53d0f1ecc3b51c6ab9809186805110fb9
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-08 10:01:27 +0300

    Added abstract and implemented classes to Animal

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Animal.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Animal.java
@@ -1,5 +1,22 @@
 package InterfacesAndInnerAndAbstractClasses.AbstractClasses;
 
 public abstract class Animal {
-    
+    private String name;
+
+    public Animal(String name){
+        this.name = name;
+    }
+
+    // Abstract methods are just method signatures which must be implemented
+    // by inheriting classes.  As far as these are concerned, an abstract
+    // class is like an interface.  The difference is that an abstract class
+    // can also provide implemented code which inheriting classes can use
+    // as-is, such as the getter found below.
+
+    public abstract void eat();
+    public abstract void breathe();
+
+    public String getName() {
+        return name;
+    }
 }
