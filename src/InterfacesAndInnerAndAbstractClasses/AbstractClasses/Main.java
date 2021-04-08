0aa35dd8ac708d03aa7bc584752d5d0db8b7ace4
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-08 10:08:12 +0300

    Added testing code to Main

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Main.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Main.java
@@ -2,6 +2,9 @@ package InterfacesAndInnerAndAbstractClasses.AbstractClasses;
 
 public class Main {
     public static void main(String[] args) {
-
+        Animal rascal = new Dog("Rascal");
+        rascal.breathe();
+        rascal.eat();
+        rascal.walk();
     }
 }
