4a8af73a3b009685a70b18bef4cc13f496cba8af
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-08 13:05:00 +0300

    Added some commentary in Main

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Main.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Main.java
@@ -5,7 +5,10 @@ public class Main {
         Animal rascal = new Dog("Rascal");
         Animal flash = new Cat("Flash");
         Bird blizzard = new Parakeet("Blizzard");
-
+        // in order to have access to fly(), we
+        // must use a Bird object rather than an
+        // Animal object
+        
         rascal.breathe();
         rascal.eat();
         rascal.walk();
