07792903e9ad5bfb541c20a6bbcb1317006c69c4
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-14 17:00:50 +0300

    Created YLinkedList class

---
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Challenge/YLinkedList.java
@@ -0,0 +1,77 @@
+package InterfacesAndInnerAndAbstractClasses.AbstractClasses.Challenge;
+
+public class YLinkedList {
+    private ListItem head;
+    private Cursor cursor;
+
+    public YLinkedList() {
+        head = null;
+        cursor = new Cursor();
+    }
+
+    private void insertBefore(ListItem existingItem, ListItem newItem) {
+        existingItem.setPrevious(newItem);
+        newItem.setNext(existingItem);
+    }
+
+    private void insertAfter(ListItem existingItem, ListItem newItem) {
+        existingItem.setNext(newItem);
+        newItem.setPrevious(existingItem);
+    }
+
+    public int length() {
+        if (head == null) return 0;
+        int count = 1;
+        ListItem currentItem = head;
+        while (currentItem.hasNext()) {
+            currentItem = currentItem.getNext();
+            count++;
+        }
+        return count;
+    }
+
+    public void addItem(ListItem newItem) {
+        if (addItemNearStartOfList(newItem)) return;
+        ListItem currentItem = head;
+        while (currentItem.hasNext()) {
+            int location = currentItem.compareTo(newItem);
+            if (location > 0) location = 1;
+            if (location < 0) location = -1;
+            switch (location) {
+                case 0:
+                    insertBefore(currentItem.getNext(), newItem);
+                    insertAfter(currentItem, newItem);
+                    return;
+                case -1:
+                    insertAfter(currentItem.getPrevious(), newItem);
+                    insertBefore(currentItem, newItem);
+                    return;
+                case 1:
+                    if (!currentItem.hasNext()) {
+                        insertAfter(currentItem, newItem);
+                        return;
+                    } else {
+                        currentItem = currentItem.getNext();
+                    }
+                    break;
+            }
+        }
+    }
+
+    private boolean addItemNearStartOfList(ListItem newItem) {
+        if (length() == 0) {
+            head = newItem;
+            return true;
+        }
+        if (newItem.compareTo(head) < 1) {
+            insertBefore(head, newItem);
+            head = newItem;
+            return true;
+        }
+        if (!head.hasNext()) {
+            insertAfter(head, newItem);
+            return true;
+        }
+        return false;
+    }
+}
