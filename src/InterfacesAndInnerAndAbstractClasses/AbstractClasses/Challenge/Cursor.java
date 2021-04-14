76a5cc8e4b186e5aaf921f5bedcfa602f0ff8274
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-14 17:01:26 +0300

    Created constructor

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Challenge/Cursor.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Challenge/Cursor.java
@@ -3,4 +3,9 @@ package InterfacesAndInnerAndAbstractClasses.AbstractClasses.Challenge;
 public class Cursor {
     private ListItem next;
     private ListItem previous;
+
+    public Cursor(){
+        next = null;
+        previous = null;
+    }
 }
