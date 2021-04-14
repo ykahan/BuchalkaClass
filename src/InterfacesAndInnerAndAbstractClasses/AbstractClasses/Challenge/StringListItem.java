a568d448c1d688aacc2ad4e2fb728c7fd21638a9
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-14 16:59:30 +0300

    Implemented compareTo()

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Challenge/StringListItem.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Challenge/StringListItem.java
@@ -4,4 +4,15 @@ public class StringListItem extends ListItem{
     public StringListItem(Object value) {
         super(value);
     }
+
+    public void addToList(){
+
+    }
+
+    @Override
+    public int compareTo(ListItem otherItem) {
+        String thisValue = (String) this.getValue();
+        String otherValue = (String) otherItem.getValue();
+        return thisValue.compareTo(otherValue);
+    }
 }
