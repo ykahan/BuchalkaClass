0ab664d144c0031481a35a8685d5bb838b120bf9
Yehoshua Kahan <yehoshua.kahan@gmail.com>
2021-04-19 13:31:55 +0300

    Created Main class with testing code

--- src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Challenge/Main.java
+++ src/InterfacesAndInnerAndAbstractClasses/AbstractClasses/Challenge/Main.java
@@ -1,2 +1,21 @@
-package InterfacesAndInnerAndAbstractClasses.AbstractClasses.Challenge;public class Main {
+package InterfacesAndInnerAndAbstractClasses.AbstractClasses.Challenge;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        YLinkedList yll = new YLinkedList();
+        ListItem name = new StringListItem("Yehoshua");
+        ListItem town = new StringListItem("Bet Shemesh");
+        ListItem familyName = new StringListItem("Kahan");
+        ListItem wife = new StringListItem("Julie");
+
+        yll.addItem(name);
+        yll.addItem(town);
+        yll.addItem(familyName);
+        yll.addItem(wife);
+
+        System.out.println(yll.toString());
+    }
 }
